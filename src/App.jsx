eb57336a26bb38d78bacbd6f981e14e642b24260
RAFiasyam <rafi.asyam23@gmail.com>
2024-05-31 21:08:04 +0700

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-[#16161A] h-[100vh] text-white items-center content-center ">
+      <section className="flex flex-row space-x-[50px]">
+        <section>
+          <h1 className="text-[180px]">RAFIASYAM.</h1>
+          <h2>Portfolio</h2>
+          <h2>Web Frontend Developer</h2>
+        </section>
+        <section className="w-[520px]">
+          <p>Here I want to present several projects that have been created while I was looking for work as a Web Frontend Developer/Frontend Developer, here I also prefer to use Reactjs to build a website and framework. Currently I am familiar with Tailwindcss and MUi (Material UI)</p>
+        </section>
+      </section>
+    </div>
   )
 }
