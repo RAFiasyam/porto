fec9c975fa100af0dd407027c55d41f98d57d204
RAFiasyam <rafi.asyam23@gmail.com>
2024-06-04 22:00:36 +0700

    link social added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 export default function App() {
   return (
     <div className="bg-[#16161A] h-[100vh] text-white items-center content-center p-[20px] ">
+      <section className="absolute top-5 flex flex-row gap-5">
+        <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCHrgmQMpVQZPGZgxcNzCZgbBnbTNrFSfGHpsRDHxkxtfbCFSMsGDcPThbnrQgxrRPrRWSRL" className="text-white opacity-25 hover:opacity-100">
+          Email
+        </a>
+        <a href="https://www.linkedin.com/in/rafi-asyam-a5bb98289/" className="text-white opacity-25 hover:opacity-100">
+          Linkedin
+        </a>
+        <a href="https://github.com/RAFiasyam" className="text-white opacity-25 hover:opacity-100">
+          Github
+        </a>
+        <a href="https://www.instagram.com/rafiasyam__/" className="text-white opacity-25 hover:opacity-100">
+          Instagram
+        </a>
+      </section>
       <section className="flex flex-row space-x-[50px]">
         <section>
           <h1 className="text-[145px] font-bold h-[200px]">RAFIASYAM.</h1>
