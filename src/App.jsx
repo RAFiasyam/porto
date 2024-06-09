d3130ed4088892fa780488a335776c62b492b8af
RAFiasyam <rafi.asyam23@gmail.com>
2024-06-09 22:50:31 +0700

    responsive clear but not realy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { ReactComponent as Logo } from './components/svg/dec.svg';
 
 export default function App() {
   return (
-    <div className="bg-[#16161A] h-[390vh] flex flex-col gap-[130px] text-white items-center content-center p-[20px] overflow-x-none sm:w-full">
+    <div className="bg-[#16161A] h-fit w-fit flex flex-col gap-[130px] text-white items-center content-center p-[20px] overflow-x-none sm:w-full xl:w-full">
       <section className="flex flex-col">
         <section className=" flex flex-row gap-5 text-[22px]">
           <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCHrgmQMpVQZPGZgxcNzCZgbBnbTNrFSfGHpsRDHxkxtfbCFSMsGDcPThbnrQgxrRPrRWSRL"
@@ -25,13 +25,13 @@ export default function App() {
             Instagram
           </a>
         </section>
-        <section className="flex flex-col gap-[30px] pt-[14vh] lg:flex-row">
+        <section className="flex flex-col gap-[30px] pt-[14vh] xl:flex-row">
           <section>
-            <h1 className="text-[145px] font-bold h-[200px]">RAFIASYAM.</h1>
-            <h2 className="text-[70px] h-[70px] text-white opacity-25 hover:opacity-100">Portfolio</h2>
-            <h2 className="text-[70px] text-white opacity-25 hover:opacity-100">Web Frontend Developer</h2>
+            <h1 className="text-[120px]  font-bold h-[200px] xl:text-[145px]">RAFIASYAM.</h1>
+            <h2 className="text-[50px] h-[70px] text-white opacity-25 hover:opacity-100 xl:text-[70px]">Portfolio</h2>
+            <h2 className="text-[50px] text-white opacity-25 hover:opacity-100 xl:text-[70px]">Web Frontend Developer</h2>
           </section>
-          <section className="w-[350px] h-[410px] text-[22px] text-justify items-center content-center text-[#94A1B2]">
+          <section className="w-[700px] h-fit text-[22px] text-justify items-center content-center text-[#94A1B2] xl:w-[350px] xl:h-[410px]">
             <p>
               Here I want to present several projects that have been created while I was looking for work as a Web Frontend Developer/Frontend Developer,
               here I also prefer to use Reactjs to build a website and framework. Currently I am familiar with Tailwindcss and MUi (Material UI).
@@ -41,18 +41,18 @@ export default function App() {
       </section>
       <section className="flex flex-col w-full gap-[25px]">
         <section className="w-full text-[22px] text-center">
-          <h1 className="text-[145px] font-bold">THEPROJECTS</h1>
+          <h1 className="text-[120px] font-bold xl:text-[145px]">WORK</h1>
         </section>
         <section className="flex flex-col items-center gap-[30px]">
-          <section className="flex flex-row gap-[90px]">
+          <section className="flex flex-col gap-[90px] lg:flex-row">
             <Co1 />
             <Co2 />
           </section>
-          <section className="flex flex-row gap-[90px]">
+          <section className="flex flex-col gap-[90px] lg:flex-row">
             <Co3 />
             <Co4 />
           </section>
-          <section className="flex flex-row gap-[90px]">
+          <section className="flex flex-col gap-[90px] lg:flex-row">
             <Co5 />
             {/* Add according to your needs👌 */}
           </section>
